a3ead70497c9cf837b9fbf2a36f7f31fa93c5603
Dennis van der Pool <dennis@demonautomatisering.nl>
2020-11-08 10:18:44 +0100

    Fixed and changed download url to Ubuntu 18.04
    Download URL in script was giving a 404 for armhf, changed url to Ubuntu 18.04, now it works
    https://partner-images.canonical.com/core/bionic/current/

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,7 +22,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://partner-images.canonical.com/core/disco/current/ubuntu-disco-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
+		wget "https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
